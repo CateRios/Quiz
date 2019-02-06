c3f56dc8a788d2bae12873d769cbf6547350b2af
CateRios <caterinarios@gmail.com>
2019-02-06 13:50:13 +0000

    Acceder a componentes del layout. Crear métodos onClick() básicos.

--- app/src/main/java/es/ulpgc/caterina/rios101/quiz/MainActivity.java
+++ app/src/main/java/es/ulpgc/caterina/rios101/quiz/MainActivity.java
@@ -2,12 +2,41 @@ package es.ulpgc.caterina.rios101.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView answerText;
+    private TextView questionText;
+    private Button trueButton, falseButton, cheatButton, nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //fijar el layout de la activity
         setContentView(R.layout.activity_main);
+
+        //acceder a componentes del layout
+        answerText = findViewById(R.id.answerText);
+        questionText = findViewById(R.id.questionText);
+
+        trueButton = findViewById(R.id.trueButton);
+        falseButton = findViewById(R.id.falseButton);
+        cheatButton = findViewById(R.id.cheatButton);
+        nextButton = findViewById(R.id.nextButton);
+    }
+
+    public void onTrueButtonClicked(View v){
+        Log.e("MainActivity", "onTrueButtonClicked()");
+        answerText.setText(R.string.trueButtonLabel);
+    }
+
+    public void onFalseButtonClicked(View v){
+        Log.e("MainActivity", "onFalseButtonClicked()");
+        answerText.setText(R.string.falseButtonLabel);
     }
 }
