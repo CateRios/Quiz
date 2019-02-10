afe5f5809321d8c694e537ed13af1f9bb19db21f
CateRios <caterinarios@gmail.com>
2019-02-10 12:29:55 +0000

    Añadir dos preguntas al Quiz. Rellenar métodos onClicked().

--- app/src/main/java/es/ulpgc/caterina/rios101/quiz/MainActivity.java
+++ app/src/main/java/es/ulpgc/caterina/rios101/quiz/MainActivity.java
@@ -28,15 +28,41 @@ public class MainActivity extends AppCompatActivity {
         falseButton = findViewById(R.id.falseButton);
         cheatButton = findViewById(R.id.cheatButton);
         nextButton = findViewById(R.id.nextButton);
+
+        questionText.setText("En el Palacio del Versalles, en París, viven varios animales. Tradicionalmente, siempre hay un cerdo llamado Napoleon");
+        answerText.setText("");
     }
 
     public void onTrueButtonClicked(View v){
-        Log.e("MainActivity", "onTrueButtonClicked()");
-        answerText.setText(R.string.trueButtonLabel);
+        if(questionText.getText().equals("En el Palacio del Versalles, en París, viven varios animales. Tradicionalmente, siempre hay un cerdo llamado Napoleon")){
+            answerText.setText("Incorrecto!");
+        }else if (questionText.getText().equals("El animal nacional de Escocia es un unicornio")) {
+            answerText.setText("Correcto!");
+        }
+        falseButton.setEnabled(false);
+        trueButton.setEnabled(false);
+
     }
 
-    public void onFalseButtonClicked(View v){
-        Log.e("MainActivity", "onFalseButtonClicked()");
-        answerText.setText(R.string.falseButtonLabel);
+    public void onFalseButtonClicked(View v) {
+        if (questionText.getText().equals("En el Palacio del Versalles, en París, viven varios animales. Tradicionalmente, siempre hay un cerdo llamado Napoleon")) {
+            answerText.setText("Correcto!");
+        }else if (questionText.getText().equals("El animal nacional de Escocia es un unicornio")) {
+            answerText.setText("Incorrecto!");
+        }
+        falseButton.setEnabled(false);
+        trueButton.setEnabled(false);
+    }
+
+
+    public void onNextButtonClicked(View view) {
+        if(!answerText.getText().equals("")){
+            answerText.setText("");
+            questionText.setText("El animal nacional de Escocia es un unicornio");
+        }
+        falseButton.setEnabled(true);
+        trueButton.setEnabled(true);
+        nextButton.setEnabled(false);
+
     }
 }
